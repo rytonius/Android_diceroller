2c479b590c46a34a847b9128bf6d71bb1071e2a1
Rytonius <89294561+rytonius@users.noreply.github.com>
2022-07-21 18:20:59 -0800

    finish and build- set as d20 dice

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,9 +2,11 @@ package com.example.diceroller
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+
+
 /**
  * This activity allows the user to roll a dice and view the result
  * on the screen.
@@ -16,11 +18,14 @@ class MainActivity : AppCompatActivity() {
 
         val rollButton: Button = findViewById(R.id.button)
         rollButton.setOnClickListener {
-            val toast = Toast.makeText(this, "Dice Rolled", Toast.LENGTH_SHORT)
-            toast.show()
-
-            rollDice()
+           /** val toast = Toast.makeText(this, "Dice Rolled", Toast.LENGTH_SHORT)
+            toast.show()*/
+            //rollDice()
+            rollD20Dice()
         }
+        // do a dice roll at open
+        //rollDice()
+        rollD20Dice()
 
     }
     /**
@@ -29,21 +34,77 @@ class MainActivity : AppCompatActivity() {
     private fun rollDice() {
         // Create new Dice object with 6 sides and roll it
         val dice = Dice(6)
-        val diceRoll = dice.Roll()
+        val diceRoll = dice.roll()
         // Update the screen with the dice roll
+        val diceImage: ImageView = findViewById(R.id.imageView)
+
+        // this will load up all the images for the results
+        val drawableResource = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> {R.drawable.dice_6}
+        }
+
+        // takes the loaded resource and sets the image in the app
+        diceImage.setImageResource(drawableResource)
+        // this is for screen readers I guess
+        diceImage.contentDescription = diceRoll.toString()
+
+        // this is for the text on the app
+        val resultTextView: TextView = findViewById(R.id.textView)
+        resultTextView.text = diceRoll.toString()
+    }
+
+    private fun rollD20Dice() {
+        val dice = Dice(20)
+        val diceRoll = dice.roll()
+        // Update the screen with the dice roll
+        val diceImage: ImageView = findViewById(R.id.imageView)
+
+        // this will load up all the images for the results
+        val drawableResource = when (diceRoll) {
+            1 -> R.drawable.d20_side_1
+            2 -> R.drawable.d20_side_2
+            3 -> R.drawable.d20_side_3
+            4 -> R.drawable.d20_side_4
+            5 -> R.drawable.d20_side_5
+            6 -> R.drawable.d20_side_6
+            7 -> R.drawable.d20_side_7
+            8 -> R.drawable.d20_side_8
+            9 -> R.drawable.d20_side_9
+            10 -> R.drawable.d20_side_10
+            11 -> R.drawable.d20_side_11
+            12 -> R.drawable.d20_side_12
+            13 -> R.drawable.d20_side_13
+            14 -> R.drawable.d20_side_14
+            15 -> R.drawable.d20_side_15
+            16 -> R.drawable.d20_side_16
+            17 -> R.drawable.d20_side_17
+            18 -> R.drawable.d20_side_18
+            19 -> R.drawable.d20_side_19
+            else -> {R.drawable.d20_side_20}
+        }
+        
+        // takes the loaded resource and sets the image in the app
+        diceImage.setImageResource(drawableResource)
+        // this is for screen readers I guess
+        diceImage.contentDescription = diceRoll.toString()
+
+        // this is for the text on the app
         val resultTextView: TextView = findViewById(R.id.textView)
         resultTextView.text = diceRoll.toString()
 
-        val diceRoll2 = dice.Roll()
-        val resultTextView2: TextView = findViewById(R.id.textView2)
-        resultTextView2.text = diceRoll2.toString()
+
     }
 
 }
 
-class Dice(val Sides: Int) {
+class Dice(private val Sides: Int) {
     // Roll that dice based on the side count
-    fun Roll(): Int {
+    fun roll(): Int {
         return (1..Sides).random()
     }
 }
